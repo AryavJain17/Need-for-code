188df85e8dae83f8f74fbb9f8c34279012b8be9c
AryavJain17 <122171980+AryavJain17@users.noreply.github.com>
2024-07-22 18:32:47 +0530

    7th

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-          <link rel="shortcut icon" href="/images/favicon.png" />
+          <link rel="icon" href="/images/favicon.ico" />
         </Head>
         <body>
           <Main />
